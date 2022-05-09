e6db0b335761eb5fae5c96d32240a1f644faea90
naviSpring <wudan@moonearly.com>
2022-05-09 08:59:22 +0800

    shiro-redis

--- blog-api/src/main/java/com/ms/blog/config/WebMvcConfig.java
+++ blog-api/src/main/java/com/ms/blog/config/WebMvcConfig.java
@@ -1,7 +1,10 @@
 package com.ms.blog.config;
 
+import com.ms.blog.handler.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -12,9 +15,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         //处理跨域配置
         registry.addMapping("/**").allowedOrigins("http://localhost:8080");
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //假设拦截test接口后续实际遇到拦截的接口是时，再配置真正的拦截接口
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login","/currentUser");
+    }
+
+
 }
